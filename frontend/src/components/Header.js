35280248271d0a8be06ff68165bf97c3262df196
ramikarkaba22 <ram.karkaba.22@heilbronn.dhbw.de>
2025-04-03 23:41:46 +0200

    Styling updates for Header component: changed text colors, background colors, and added active link styles.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,18 @@
 // Header.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './styling/Header.css';
 import MapIcon from '@mui/icons-material/Map';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-
+{/* import logo from "./TravelBuddyLogo.png"; */}
 
 function Header() {
+  const location = useLocation();
   return (
     <header>
       <h1>
+        {/* <img src={logo} className="logo" /> */}
         <span className="travel">Travel</span>
         <span className="buddy">Buddy</span>
         <br/>
@@ -18,19 +20,19 @@ function Header() {
       </h1>
       <nav>
         <ul>
-          <li>
+          <li className={location.pathname === "/inspiration" ? "active" : ""}>
             <Link to="/inspiration">
               <AutoAwesomeIcon className='autoAwesomeIcon'/> 
               Lass dich Inspirieren!
             </Link>
           </li>
-          <li>
+          <li className={location.pathname === "/my_travel_plans" ? "active" : ""}>
             <Link to="/my_travel_plans">
               <CalendarMonthIcon className='calendarMonthIcon'/>
               Meine Reisepläne
             </Link>
           </li>
-          <li>
+          <li className={location.pathname === "/" ? "active" : ""}>
             <Link to="/">
               <MapIcon />
             </Link>
