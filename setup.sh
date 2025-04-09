4a93a53ff2aa0edc75c3487f955d2ec20be863f5
Oliver <116276047+oliverp28@users.noreply.github.com>
2025-04-09 21:45:23 +0200

    add setup.sh file
    use command: bash setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Setup für AI Travel Buddy Projekt
+
+echo "Erstelle virtuelle Umgebung..."
+python3 -m venv venv
+
+echo "Aktiviere virtuelle Umgebung..."
+source venv/bin/activate
+
+echo "Installiere Python-Abhaengigkeiten..."
+pip install -r requirements.txt
+
+echo "Starte main.py ..."
+python3 backend/main.py
+
+echo "Starte FastAPI-Server mit Uvicorn..."
+uvicorn backend.main:app --reload
